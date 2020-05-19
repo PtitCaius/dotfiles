8c5df6e76548e10067f99c43c0acf6acc9af831b
Marius Lopez <marius.lopez@damavan-imaging.com>
2020-05-19 10:15:13 +0200

    Adding locking shortcut

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -340,6 +340,8 @@ globalkeys = gears.table.join(
               {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
+    awful.key({ modkey, }, "l", function() awful.spawn(locker) end,
+              {description = "lock screen", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
